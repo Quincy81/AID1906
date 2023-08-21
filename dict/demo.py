253f3934830f49d65bd00218205ea3d486a92a6e
quincy <5036602@qq.com>
2023-08-21 12:38:24 +0800

    dict

---
+++ dict/demo.py
@@ -0,0 +1,9 @@
+while True:
+    print('界面1')
+    cmd = input()
+    if cmd == 'in':
+        while True:
+            print('二级界面')
+            cmd = input()
+            if cmd == 'out':
+                break
